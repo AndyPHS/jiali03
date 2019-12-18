2edcec7ff139f3194dc9adf9ad42d2701d16ba00
patterson <1479009707@qq.com>
2019-12-18 11:38:17 +0800

    新增组合弹出

--- src/components/DisplayRule.vue
+++ src/components/DisplayRule.vue
@@ -422,6 +422,10 @@
             
             addWordOk () {   // 点击新增组合确定按钮提交表单
                 this.wordAdd.where.push(this.wordAddWhere) // 提交组合绑定的问题
+                for(let i = 0;i<this.wordAdd.where.length;i++){
+                  this.$delete(this.wordAdd.where[i], 'label')
+                  this.$delete(this.wordAdd.where[i], 'title')
+                }
                 this.wordAdd.where = JSON.stringify(this.wordAdd.where)
                 if(this.wordTreeMsg.fqaspId == null){
                     addWord({
