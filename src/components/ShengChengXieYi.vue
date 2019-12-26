57ec47ec63557e428994a82644344b981bf7359c
patterson <1479009707@qq.com>
2019-12-26 10:33:24 +0800

    添加生成页面样式

--- src/components/ShengChengXieYi.vue
+++ src/components/ShengChengXieYi.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="container mx-auto">
-    <div>
-
-      <div class="outputword rounded-lg shadow-lg mt-5 relative">
-        <h2 class="py-2 text-xl">离婚协议书</h2>
-        <div class="absolute top-0 right-0 mt-2 mr-2">
-          <el-button class="text-right" type="primary" @click="GoBasicInformationPage">返回修改</el-button>
-        </div>
-        <div class="w-full">
-          <div class="text-left px-2 py-2 msg">
-            <div v-if="this.status_code ==330">
-              <h2>以下<span class="text-red-500">必填项</span>未填写，请<span  @click="GoBasicInformationPage" class="text-blue-600">返回继续填写</span></h2>
-              <ul>
-                <li v-for="(item, index) in missField" :key="index">{{index+1}}、{{item.problemTitle}}</li>
-              </ul>
-            </div>
-            <div v-if="this.status_code ==200">
-              {{outputWord}}
+  <div class="w-full bg-color">
+    <div class="container mx-auto py-10">
+      <div class="bg-white rounded-lg">
+        <div class="outputword rounded-lg shadow-lg relative">
+          <h2 class="py-2 text-2xl font-bold">离婚协议书</h2>
+          <div class="absolute top-0 right-0 mt-4 mr-6">
+            <el-button class="text-right" type="primary" @click="GoBasicInformationPage">返回修改</el-button>
+          </div>
+          <div class="w-full">
+            <div class="text-left px-4 py-3 msg">
+              <div v-if="this.status_code ==330">
+                <h2>以下<span class="text-red-500">必填项</span>未填写，请<span  @click="GoBasicInformationPage" class="text-blue-600">返回继续填写</span></h2>
+                <ul>
+                  <li v-for="(item, index) in missField" :key="index">{{index+1}}、{{item.problemTitle}}</li>
+                </ul>
+              </div>
+              <div v-if="this.status_code ==200">
+                {{outputWord}}
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+  
 </template>
 <script>
 
@@ -63,7 +65,8 @@
     }
 </script>
 <style scoped>
-.outputword{padding:10px;border:1px solid #ecf5ec;}
+.bg-color{background: #eee;}
+.outputword{padding:20px;border:1px solid #ecf5ec;}
 .outputword>h2{border-bottom: 1px solid #dbe2db;}
-.outputword .msg{white-space:pre-wrap;}
+.outputword .msg{white-space:pre-wrap;margin-top: -50px;}
 </style>
