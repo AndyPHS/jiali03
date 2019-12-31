3ca6157aafdd2504c94daec3f82312591d9cc604
patterson <1479009707@qq.com>
2019-12-31 13:53:17 +0800

    添加用户管理排序

--- src/components/HeadMenu.vue
+++ src/components/HeadMenu.vue
@@ -11,22 +11,15 @@
             <!--<el-menu-item index="1">用户管理</el-menu-item>-->
             <el-submenu index="1">
                 <template slot="title">用户管理</template>
-                <el-menu-item index="2-1"><router-link  to="Users">管理员列表</router-link></el-menu-item>
-                <el-menu-item index="2-2"><router-link  to="UsersPermission">权限列表</router-link></el-menu-item>
-                <el-menu-item index="2-3"><router-link  to="UsersRole">角色列表</router-link></el-menu-item>
+                <el-menu-item index="1-1"><router-link  to="Users">管理员列表</router-link></el-menu-item>
+                <el-menu-item index="1-2"><router-link  to="UsersPermission">权限列表</router-link></el-menu-item>
+                <el-menu-item index="1-3"><router-link  to="UsersRole">角色列表</router-link></el-menu-item>
             </el-submenu>
             <el-submenu index="2" collapse-transition>
                 <template slot="title">我的工作台</template>
                 <el-menu-item index="2-1"><router-link to="CaseUpload">上传案件</router-link></el-menu-item>
                 <el-menu-item index="2-2"><router-link to="FileList">案件列表</router-link></el-menu-item>
                  <el-menu-item index="2-3" class="hidden"><router-link to="CaseCourt">法院管理</router-link></el-menu-item>
-                <!--<el-menu-item index="2-3"></el-menu-item>-->
-                <!--<el-submenu index="2-4">-->
-                    <!--<template slot="title">选项4</template>-->
-                    <!--<el-menu-item index="2-4-1">选项1</el-menu-item>-->
-                    <!--<el-menu-item index="2-4-2">选项2</el-menu-item>-->
-                    <!--<el-menu-item index="2-4-3">选项3</el-menu-item>-->
-                <!--</el-submenu>-->
             </el-submenu>
             <el-menu-item index="3"><router-link to="FileList">案件列表</router-link></el-menu-item>
             <el-menu-item index="4"><router-link to="SearchCase">案例检索</router-link></el-menu-item>
